099003010cedfaea8e7a9966cd41f7013863b277
rajalakshmi-mastek <rajalakshmi.k@mastek.com>
2026-06-01 10:33:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import os
 from snowflake.snowpark.functions import col,when_matched
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f"Custom Your Smoothie")
@@ -16,8 +17,13 @@ st.write("The Name on your Smoothie will be", name_on_order)
 cnx=st.connection("snowflake")
 session=cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')),col('SEARCH_ON')
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
+
 
 ingredients_list = st.multiselect(
     "Choose upto 5 ingridents:",
@@ -30,6 +36,8 @@ if ingredients_list:
 
    for fruit_chosen in ingredients_list:
         ingredient_string+=fruit_chosen+' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen+ 'Nutrition Information')
         smoothiefroot_response = requests.get(
             "https://my.smoothiefroot.com/api/fruit/"+fruit_chosen
